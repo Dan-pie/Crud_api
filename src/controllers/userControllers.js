0c41b4a39472c5e1ad7e59e194c58aec26363f6b
Dan-pie <dann.ferreira.carvalho@gmail.com>
2026-04-15 13:02:58 -0300

    add local db

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -2,18 +2,21 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { v4 } = require("uuid");
 const users = [];
+const db = require("../config/db");
 const JWT_SECRET = process.env.JWT_SECRET;
 
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET não definido");
 }
 
-function generateToken(user){
-  return jwt.sign({ id: user.id, email: user.email, username: user.username },
-      JWT_SECRET,
-      {
-        expiresIn: "1h",
-      },)
+function generateToken(user) {
+  return jwt.sign(
+    { id: user.id},
+    JWT_SECRET,
+    {
+      expiresIn: "1h",
+    },
+  );
 }
 
 exports.signUp = async (req, res) => {
@@ -24,24 +27,25 @@ exports.signUp = async (req, res) => {
         .status(400)
         .json({ error: "Todos os campos tem que estar preenchidos" });
     }
-    const existingUser = users.find((u) => u.email === email);
-    if (existingUser)
-      return res.status(400).json({ error: "Usuário já cadastrado" });
 
     const newPassword = await bcrypt.hash(password, 10);
 
-    const user = { id: v4(), email, username, password: newPassword };
-    users.push(user);
+    const result = await db.query(
+      "INSERT INTO users (email, username, password) VALUES ($1, $2, $3) RETURNING *",
+      [email, username, newPassword],
+    );
 
-    const token = generateToken(user);
+    const newUser = result.rows[0];
+
+    const token = generateToken(newUser);
 
     res.status(201).json({
       message: "Usuário criado com sucesso!",
       token,
       user: {
-        id: user.id,
-        email: user.email,
-        username: user.username,
+        id: newUser.id,
+        email: newUser.email,
+        username: newUser.username,
       },
     });
   } catch (err) {
@@ -54,8 +58,8 @@ exports.update = async (req, res) => {
     const id = req.user.id;
     const { username, password } = req.body;
 
-    const user = users.find((u) => u.id === id);
-    if (!user) {
+    const user = await db.query("SELECT * FROM users WHERE id = $1", [id]);
+    if (user.rows.length === 0) {
       return res.status(404).json({ error: "Usuário não encontrado" });
     }
 
@@ -63,18 +67,33 @@ exports.update = async (req, res) => {
       return res.status(400).json({ error: "Nada para atualizar" });
     }
 
-    if (username) user.username = username;
+    let query = "UPDATE users SET ";
+    let values = [];
+    let index = 1;
+
+    if (username) {
+      query += `username = $${index}, `;
+      values.push(username);
+      index++;
+    }
 
     if (password) {
-      user.password = await bcrypt.hash(password, 10);
+      const hash = await bcrypt.hash(password, 10);
+      query += `password = $${index}`;
+      values.push(hash);
+      index++;
     }
+
+    query = query.slice(0, -2);
+
+    query += ` WHERE id = $${index} RETURNING id, username, email`;
+    values.push(id);
+
+    const result = await db.query(query, values);
 
     res.json({
       message: "Atualizado com sucesso",
-      user: {
-        id: user.id,
-        username: user.username,
-      },
+      user: result.rows[0],
     });
   } catch (err) {
     res.status(500).json({ error: "Erro interno do servidor" });
@@ -89,11 +108,15 @@ exports.signIn = async (req, res) => {
       return res.status(400).json({ error: "Campos inválidos" });
     }
 
-    const user = users.find((u) => u.email === email);
-    if (!user) {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (result.rows.length === 0) {
       return res.status(401).json({ error: "Email ou senha inválidos." });
     }
 
+    const user = result.rows[0];
     const valid = await bcrypt.compare(password, user.password);
 
     if (!valid)
@@ -107,13 +130,19 @@ exports.signIn = async (req, res) => {
   }
 };
 
-exports.delete = (req, res) => {
-  const id = req.user.id;
-  const index = users.findIndex((u) => u.id === id);
-  if (index === -1) {
-    return res.status(404).json({ error: "Usuário não encontrado" });
-  }
+exports.delete = async (req, res) => {
+  try {
+    const id = req.user.id;
+    const result = await db.query(
+      "DELETE FROM users WHERE id = $1 RETURNING id",
+      [id],
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Usuário não encontrado" });
+    }
 
-  users.splice(index, 1);
-  res.json({ message: "Usuário deletado com sucesso" });
+    res.json({ message: "Usuário deletado com sucesso" });
+  } catch (err) {
+    res.status(500).json({ error: "Erro interno do servidor" });
+  }
 };
